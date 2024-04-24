8941da54a0e01207b48138f18288434b7f090848
Mehdi <78212042+m-aissi@users.noreply.github.com>
2024-04-25 00:25:27 +0200

    final push

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if cuneiform_input == '1':
             print(cuneiform_to_alphabet[each], end='')
             
 if cuneiform_input == '2':
-    shadow_spell_type = input('Select shadow spell type: \n 1. Curse of Mending \n 2. Curse of Mending \n')
+    shadow_spell_type = input('Select shadow spell type: \n 1. Curse of Mending \n 2. Curse of Vanishing \n')
     if(shadow_spell_type == '1'):
         shadow_spell = "Maledictum vinculi aeterni Ludio loricam tollere iam non potest".lower()
         spell_tab = shadow_spell.split()
@@ -30,7 +30,7 @@ if cuneiform_input == '2':
             # spell_translation.append(' ')
         print(''.join(spell_translation))
     elif(shadow_spell_type == '2'):
-        curse_mending = 'Maledicta ablatione Perimit item cum ludio ludius moritur'.lower()
+        curse_mending = 'MaledictaablationePerimititemcumludioludiusmoritur'.lower()
         for char in curse_mending:
             print(cuneiform_to_alphabet[char], end='')
 if cuneiform_input == '3':
@@ -38,7 +38,7 @@ if cuneiform_input == '3':
     for char in fire_spell:
         print(cuneiform_to_alphabet[char], end='')
 if cuneiform_input == '4':
-    frost_spell = 'Obscurum Aeternum In abysso superbi peccato primario gloriamur'.lower()
+    frost_spell = 'ObscurumAeternumInabyssosuperbipeccatoprimariogloriamur'.lower()
     for char in frost_spell:
         print(cuneiform_to_alphabet[char], end='')
     
